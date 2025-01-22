99b66b900ff078a37d1381cd20dfe7a5d8cea6bd
junpreetham <arjunpreetham49@gmail.com>
2025-01-22 13:13:35 +0800

    Review fixes

--- arjunpreetham49-relay-datasource/build.sh
+++ arjunpreetham49-relay-datasource/build.sh
@@ -8,27 +8,27 @@ echo "Creating plugin binaries..."
 webpack -c ./.config/webpack/webpack.config.ts --env production 
 mage 
 
-echo "Updating README..."
-rm dist/README.md
-cp README.md dist/README.md
+# echo "Updating README..."
+# rm dist/README.md
+# cp README.md dist/README.md
 
-echo "Creating release..."
-mkdir releases/arjunpreetham49-relay-datasource
-mv dist/* releases/arjunpreetham49-relay-datasource
+# echo "Creating release..."
+# mkdir releases/arjunpreetham49-relay-datasource
+# mv dist/* releases/arjunpreetham49-relay-datasource
 
-echo "Moving GO files..."
-cp go.mod releases/arjunpreetham49-relay-datasource
-cp Magefile.go releases/arjunpreetham49-relay-datasource
-cp -r pkg releases/arjunpreetham49-relay-datasource
+# echo "Moving GO files..."
+# cp go.mod releases/arjunpreetham49-relay-datasource
+# cp Magefile.go releases/arjunpreetham49-relay-datasource
+# cp -r pkg releases/arjunpreetham49-relay-datasource
 
-echo "Ziping up plugin..."
-cd releases/
-zip arjunpreetham49-relay-datasource-$PLUGIN_VERSION.zip . -r --exclude .DS_Store
-rm -r arjunpreetham49-relay-datasource
-zipinfo arjunpreetham49-relay-datasource-$PLUGIN_VERSION.zip
-cd ../ 
-rm -r dist
-MD5_HASH=$(md5 releases/arjunpreetham49-relay-datasource-$PLUGIN_VERSION.zip)
+# echo "Ziping up plugin..."
+# cd releases/
+# zip arjunpreetham49-relay-datasource-$PLUGIN_VERSION.zip . -r --exclude .DS_Store
+# rm -r arjunpreetham49-relay-datasource
+# zipinfo arjunpreetham49-relay-datasource-$PLUGIN_VERSION.zip
+# cd ../ 
+# rm -r dist
+# MD5_HASH=$(md5 releases/arjunpreetham49-relay-datasource-$PLUGIN_VERSION.zip)
 echo "Done"
 
-echo "MD5 Hash => $MD5_HASH"
+# echo "MD5 Hash => $MD5_HASH"
